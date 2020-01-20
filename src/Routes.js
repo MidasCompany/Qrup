c2c6d22601a2c112dcb5d1168a4edbe1ea44c259
pauloarn <paulo.amador98@gmail.com>
2020-01-20 11:03:51 -0300

    MaterialSwipableBottomTab
    Implemented SwipableBottomTab Navigation

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,18 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Login from './Login'
-import Root from './Root'
+import Root_ from './Root_'
 import Reader from './Reader'
 import Register from './Register'
 
 const App = createStackNavigator({
           Login: Login,
-          User: Root,  
+          User: Root_,  
           Reader: Reader,
           Register: Register,
         },
         {
-          initialRouteName: 'Login',
+          initialRouteName: 'User',
           headerMode: 'none',
           navigationOptions: {
            headerVisible: false,

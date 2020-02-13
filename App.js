004b39b0055440069acf8fe7468ecc2b8b160af3
pauloarn <44753954+pauloarn@users.noreply.github.com>
2020-02-12 22:31:42 -0300

    Bugs

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {StatusBar} from 'react-native'
 import Routes from './src/Routes';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export default function App() {
-  	return (
-		<>
-			<StatusBar backgroundColor = "#006300" barStyle="light-content" />
-			<Routes />
-		</>
-	)
+export default class App extends React.Component {
+	render(){
+		return (
+			<>
+				<StatusBar backgroundColor = "#006300" barStyle="light-content" />
+				<Routes />
+			</>
+		)
+	}
 }
